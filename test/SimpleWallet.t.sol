8dc13a92052a455f5e4f5b6fc31e4de4dbdc6742
Stepan <stepangera15@gmail.com>
2024-12-19 17:53:19 +0300

    readme+t.sol update

--- test/SimpleWallet.t.sol
+++ test/SimpleWallet.t.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.28;
 
 import "forge-std/Test.sol";
-import "src/SimpleWallet.sol"; // путь к вашему контракту
+import "src/SimpleWallet.sol"; // путь к контракту
 
 contract SimpleWalletTest is Test {
     SimpleWallet wallet;
@@ -10,7 +10,7 @@ contract SimpleWalletTest is Test {
     address user = address(0x123); // сторонний пользователь
 
     function setUp() public {
-        wallet = new SimpleWallet(); // Деплой контракта
+        wallet = new SimpleWallet(); // деплой контракта
     }
 
     function testOwnerInitialization() public {
@@ -19,47 +19,47 @@ contract SimpleWalletTest is Test {
 
     function testReceiveFunds() public {
         uint initialBalance = wallet.getBalance();
-        vm.deal(address(this), 1 ether); // Выдать баланс тестовому контракту
-        payable(address(wallet)).transfer(0.5 ether); // Перевод на контракт
+        vm.deal(address(this), 1 ether); // выдать баланс тестовому контракту
+        payable(address(wallet)).transfer(0.5 ether); // перевод на контракт
 
         assertEq(wallet.getBalance(), initialBalance + 0.5 ether, "Balance not changes");
     }
 
     function testTransfer() public {
-        vm.deal(address(wallet), 1 ether); // Установить баланс контракта
+        vm.deal(address(wallet), 1 ether); // установить баланс контракта
         uint initialUserBalance = user.balance;
 
-        wallet.transfer(payable(user), 0.5 ether); // Перевод средств
+        wallet.transfer(payable(user), 0.5 ether); // перевод средств
 
         assertEq(user.balance, initialUserBalance + 0.5 ether, "The funds have not been received by the user");
         assertEq(wallet.getBalance(), 0.5 ether, "No funds were debited from the contract");
     }
 
     function testWithdraw() public {
-        vm.deal(address(wallet), 1 ether); // Установить баланс контракта
+        vm.deal(address(wallet), 1 ether); // установить баланс контракта
         uint initialOwnerBalance = owner.balance;
 
-        wallet.withdraw(0.5 ether); // Вывод средств
+        wallet.withdraw(0.5 ether); // вывод средств
 
         assertEq(owner.balance, initialOwnerBalance + 0.5 ether, "The funds have not been received by the owner");
         assertEq(wallet.getBalance(), 0.5 ether, "No funds were debited from the contract");
     }
 
     function testFailNonOwnerTransfer() public {
-        vm.prank(user); // Изменить msg.sender на user
-        wallet.transfer(payable(user), 0.5 ether); // Ожидается ошибка
+        vm.prank(user); // изменить msg.sender на user
+        wallet.transfer(payable(user), 0.5 ether);
     }
 
     function testFailNonOwnerWithdraw() public {
-        vm.prank(user); // Изменить msg.sender на user
-        wallet.withdraw(0.5 ether); // Ожидается ошибка
+        vm.prank(user); // изменить msg.sender на user
+        wallet.withdraw(0.5 ether);
     }
 
     function testFailInsufficientBalanceTransfer() public {
-        wallet.transfer(payable(user), 1 ether); // Попытка перевода при недостаточном балансе
+        wallet.transfer(payable(user), 1 ether); // попытка перевода при недостаточном балансе
     }
 
     function testFailInsufficientBalanceWithdraw() public {
-        wallet.withdraw(1 ether); // Попытка вывода при недостаточном балансе
+        wallet.withdraw(1 ether); // попытка вывода при недостаточном балансе
     }
 }
